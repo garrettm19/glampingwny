c0e92ce0e20210ebf859e3390ca1df34045d0493
Mason Garrett <94884228+garrettm19@users.noreply.github.com>
2025-06-15 21:22:45 -0400

    Fix ServiceCard import path

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import ServiceCard from '../components/ui/ServiceCard';
+import ServiceCard from '../ui/ServiceCard';
 import { trackEvent } from '../../utils/analytics';
 
 const indoorPackages = [
